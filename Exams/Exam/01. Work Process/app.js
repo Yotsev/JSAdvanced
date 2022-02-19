fb7d906d7d51c0e1c8b2ba8d444e06b95299aafc
Yotsev <b.yotsev@gmail.com>
2022-02-19 16:38:12 +0200

    Small change to the solution

--- Exams/Exam/01. Work Process/app.js
+++ Exams/Exam/01. Work Process/app.js
@@ -8,7 +8,9 @@ function solve() {
     const hireButtonElement = document.getElementById('add-worker');
     const tableElement = document.getElementById('tbody');
     const budgetSumElement = document.getElementById('sum');
-
+    const firedBtnElement = document.createElement('button');
+    const editBtnElement = document.createElement('button');
+   
     hireButtonElement.addEventListener('click', (e) => {
         e.preventDefault();
 
@@ -20,8 +22,7 @@ function solve() {
         const positionRowElement = document.createElement('td');
         const salaryRowElement = document.createElement('td');
         const buttonsRowElement = document.createElement('td');
-        const firedBtnElement = document.createElement('button');
-        const editBtnElement = document.createElement('button');
+        
 
         firtstNameRowElement.textContent = fNameElement.value;
         lastNameRowElement.textContent = lNameElemenet.value;
@@ -61,32 +62,34 @@ function solve() {
         positionElement.value = '';
         salaryElement.value = '';
 
-        editBtnElement.addEventListener('click', (e) => {
-            const infoToEdit = Array.from(e.target.parentElement.parentElement.childNodes);
-    
-            fNameElement.value = infoToEdit[0].textContent;
-            lNameElemenet.value = infoToEdit[1].textContent;
-            emailElement.value = infoToEdit[2].textContent;
-            birthlElement.value = infoToEdit[3].textContent;
-            positionElement.value = infoToEdit[4].textContent;
-            salaryElement.value = infoToEdit[5].textContent;
-    
-            let currentBudget = Number(budgetSumElement.textContent);
-            let sum = currentBudget - Number(salaryElement.value);
-            budgetSumElement.textContent = sum.toFixed(2);
-            
+        
+    });
 
-            e.target.parentElement.parentElement.remove();
-        });
+    editBtnElement.addEventListener('click', (e) => {
+        const infoToEdit = Array.from(e.target.parentElement.parentElement.childNodes);
 
-        firedBtnElement.addEventListener('click', (e)=>{
-            const infoToEdit = Array.from(e.target.parentElement.parentElement.childNodes);
-            let currentBudget = Number(budgetSumElement.textContent);
-            let sum = currentBudget - Number(infoToEdit[5].textContent)
-            
-            budgetSumElement.textContent = sum.toFixed(2);
-            e.target.parentElement.parentElement.remove();
-        });
+        fNameElement.value = infoToEdit[0].textContent;
+        lNameElemenet.value = infoToEdit[1].textContent;
+        emailElement.value = infoToEdit[2].textContent;
+        birthlElement.value = infoToEdit[3].textContent;
+        positionElement.value = infoToEdit[4].textContent;
+        salaryElement.value = infoToEdit[5].textContent;
+
+        let currentBudget = Number(budgetSumElement.textContent);
+        let sum = currentBudget - Number(salaryElement.value);
+        budgetSumElement.textContent = sum.toFixed(2);
+        
+
+        e.target.parentElement.parentElement.remove();
+    });
+
+    firedBtnElement.addEventListener('click', (e)=>{
+        const infoToEdit = Array.from(e.target.parentElement.parentElement.childNodes);
+        let currentBudget = Number(budgetSumElement.textContent);
+        let sum = currentBudget - Number(infoToEdit[5].textContent)
+        
+        budgetSumElement.textContent = sum.toFixed(2);
+        e.target.parentElement.parentElement.remove();
     });
 }
 solve()
